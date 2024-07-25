13cc1d6929f0d72d27e15db9141b4fcc0b4797cc
Mathias Jeschke <mjeschke@juniper.net>
2024-07-25 14:43:07 +0200

    Improved template handling and absence of variables

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 import argparse
-from flask import Flask, render_template, render_template_string
-import jinja2
+import collections.abc
+import os
+import pkgutil
 import requests
-from textwrap import indent
+import sys
 import yaml
+from flask import Flask, render_template, render_template_string
+from jinja2 import Environment, FunctionLoader
+from textwrap import indent
+
+from templates.defaults import defaults
 
 DEFAULT_DNS = [ '8.8.8.8', '1.1.1.1' ]
-TEMPLATE_PATH = '/var/lib/t128-nocloud-net/templates'
+MAX_IP = 250
 
 app = Flask(__name__)
 
@@ -22,13 +28,47 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def load_template(vm_type):
+    # if file cannot be found (type == None) return a 404
+    data = None
+
+    # first try to load a template from program directory (templates)
+    # if not possible, try to load it from the pyz
+    dir = os.path.join(os.path.dirname(sys.argv[0]), 'templates')
+    filename = f'{vm_type}.yaml'
+    if vm_type.endswith('.yaml'):
+        filename = vm_type
+    try:
+        with open(os.path.join(dir, filename)) as fd:
+            data = fd.read()
+            return data
+    except FileNotFoundError:
+        pass
+
+    # try to load template from pyz
+    try:
+        data = pkgutil.get_data('templates', filename).decode('utf8')
+    except OSError:
+        data = f'# Could not find a template for machine type: {vm_type}\n\n'
+    return data
+
+
 def load_deployment(url):
     r = requests.get(url)
     if r.status_code == 200:
         return yaml.safe_load(r.text)
 
 
-def get_network_config(deployment_config, vm_name):
+def update(d, u):
+    for k, v in u.items():
+        if isinstance(v, collections.abc.Mapping):
+            d[k] = update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
+
+
+def get_network_config(deployment_config, vm_name, mgmt_ip):
     network_config = {
         'network': {
             'version': 2,
@@ -40,7 +80,7 @@ def get_network_config(deployment_config, vm_name):
         if vm.get('name') == vm_name:
             vm_id = vm.get('id')
             network_config['network']['ethernets']['eth0'] = {
-                 'addresses': [ mgmt_ip_prefix.format(vm_id) + '/24' ],
+                 'addresses': [ mgmt_ip + '/24' ],
                  'gateway4': mgmt_ip_prefix.format(1),
                  'nameservers': {
                     'addresses': DEFAULT_DNS,
@@ -78,8 +118,10 @@ local-hostname: {hostname}
 @app.route('/<hostname>/user-data')
 def user(hostname):
     global mgmt_ip_prefix
-    deployment_config = load_deployment(app.config['DEPLOYMENT_URL'])
-    mgmt_ip_prefix = deployment_config['global']['mgmt_ip_prefix'] + '{}'
+    deployment_config = defaults
+    update(deployment_config, load_deployment(app.config['DEPLOYMENT_URL']))
+    mgmt_ip_prefix = deployment_config['global']['mgmt_ip_prefix'].strip('.') + '.{}'
+
     user_data = ''
     for vm in deployment_config.get('vms'):
         vm_name = vm.get('name')
@@ -87,11 +129,18 @@ def user(hostname):
         vm_id = vm.get('id')
         deployment_name = deployment_config['deployment']
         if hostname == f'{vm_name}' or hostname == f'{deployment_name}-{vm_name}':
-            network_config = indent(get_network_config(deployment_config, vm_name), ' '*4)
+            host_byte = vm_id
+            if host_byte > MAX_IP:
+                host_byte = host_byte % (MAX_IP - 1) + 2
+            mgmt_ip = mgmt_ip_prefix.format(host_byte)
+            network_config = indent(get_network_config(deployment_config, vm_name, mgmt_ip), ' '*4)
             if vm_type:
-                mgmt_ip = mgmt_ip_prefix.format(vm_id)
-                conductor_ips = [mgmt_ip_prefix.format(c) for c in deployment_config['global']['ssr']['conductor']]
-                template = env.get_template(f'{vm_type}.yaml')
+                conductor_ips = []
+                try:
+                    conductor_ips = [mgmt_ip_prefix.format(c) for c in deployment_config['global']['ssr']['conductor']]
+                except KeyError:
+                    pass
+                template = env.get_template(vm_type)
                 addition_variables = {}
                 addition_variables.update(deployment_config['global'])
                 if vm.get('template_variables'):
@@ -117,8 +166,7 @@ def vendor(hostname):
 
 if __name__ == '__main__':
     global env
-    loader = jinja2.FileSystemLoader([TEMPLATE_PATH, 'templates'])
-    env = jinja2.Environment(loader=loader, cache_size=0)
+    env = Environment(loader=FunctionLoader(load_template), cache_size=0)
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     args = parse_arguments()
     app.config['DEPLOYMENT_URL'] = args.deployment
---
+++ templates/defaults.py
@@ -0,0 +1,10 @@
+defaults = {
+    'global': {
+        'mgmt_ip_prefix': '100.100.100',
+        'admin_password': '128tRoutes',
+        'root_password': '128tRoutes',
+        'options': {
+            'citype': 'nocloud',
+        },
+    },
+}
