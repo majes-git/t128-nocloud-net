bba0c56a5db3450bcc7125f678e46f2182fc0aa1
Mathias Jeschke <mjeschke@juniper.net>
2024-10-09 12:48:14 +0200

    Changed default admin password

--- templates/defaults.py
+++ templates/defaults.py
@@ -1,7 +1,7 @@
 defaults = {
     'global': {
         'mgmt_ip_prefix': '100.100.100',
-        'admin_password': '128tRoutes',
+        'admin_password': '128tRoutes.1',
         'root_password': '128tRoutes',
         'options': {
             'citype': 'nocloud',
