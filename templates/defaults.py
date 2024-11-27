9933a9adc5bb110f142adacce6c736406b84d6ba
Mathias Jeschke <mjeschke@juniper.net>
2024-11-27 20:06:32 +0100

    Fixed default SSR admin password

--- templates/defaults.py
+++ templates/defaults.py
@@ -1,7 +1,7 @@
 defaults = {
     'global': {
         'mgmt_ip_prefix': '100.100.100',
-        'admin_password': '128tRoutes.1',
+        'admin_password': '$6$W2wtGSOP7lT4vqLj$zG3eYb9/QAWD/0PqiN/I6vVLtDhBzhecx.aBpdweIx4D.0NJzxIUGVRRBQPmO9K8LlU/Jj8iBaL3OS7aa75KD1',
         'root_password': '128tRoutes',
         'options': {
             'citype': 'nocloud',
