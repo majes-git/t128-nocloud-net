cae49f4dbbfb499203c170cc2b68d789477adc67
Mathias Jeschke <mjeschke@juniper.net>
2024-11-29 12:18:28 +0100

    Fixed missing default credentials for HA conductor

--- templates/defaults.py
+++ templates/defaults.py
@@ -3,6 +3,8 @@ defaults = {
         'mgmt_ip_prefix': '100.100.100',
         'admin_password': '$6$W2wtGSOP7lT4vqLj$zG3eYb9/QAWD/0PqiN/I6vVLtDhBzhecx.aBpdweIx4D.0NJzxIUGVRRBQPmO9K8LlU/Jj8iBaL3OS7aa75KD1',
         'root_password': '128tRoutes',
+        'ha_peer_username': 't128',
+        'unsafe_ha_peer_password': '128tRoutes',
         'options': {
             'citype': 'nocloud',
         },
